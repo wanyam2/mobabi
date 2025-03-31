84a8cc3497a2476f68d73eca8349f5df4d058c43
EUNCHAE <2snha@naver.com>
2025-03-31 20:59:32 +0900

    feat: branch 상태 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import './App.css';
 import SideBar from "./components/SideBar/SideBar.jsx";
 import BranchContainer from "./components/BranchContainer/BranchContainer.jsx";
 import ButtonContainer from "./components/ButtonContainer/ButtonContainer.jsx";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
+import React, { useState } from 'react';
 
 function App() {
+    const [branches, setBranches] = useState([
+        { name: "main", pushedCommits: [{ id: 1, message: "Initial commit" }] },
+        { name: "develop", pushedCommits: [{ id: 1, message: "Started new feature Y" }] },
+    ]);
 
     return (
         <ChakraProvider>
             <div className="App">
-                <SideBar/>
-                <BranchContainer />
-                <ButtonContainer />
+                <SideBar />
+                <BranchContainer branches={branches} />
+                <ButtonContainer branches={branches} setBranches={setBranches} />
             </div>
         </ChakraProvider>
     );
--- src/components/BranchContainer/BranchContainer.jsx
+++ src/components/BranchContainer/BranchContainer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "./BranchContainer.css"
+import CommitGraph from "../CommitGraph/CommitGraph"; // CommitGraph 컴포넌트 임포트
+import "./BranchContainer.css";
 
-
-export default function BranchContainer() {
+export default function BranchContainer({ branches }) {
     return (
         <div className="BranchContainer">
+            <CommitGraph branches={branches} />
         </div>
     );
 }
