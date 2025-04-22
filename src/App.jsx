8a6177ad8af2b201b4b483c1a331e6d160f00e1b
EUNCHAE <2snha@naver.com>
2025-04-22 14:15:28 +0900

    refactor: Component -> page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import BranchContainer from "./components/BranchContainer/BranchContainer.jsx";
-import ButtonContainer from "./components/ButtonContainer/ButtonContainer.jsx";
+import BranchContainer from "@/page/BranchContainer/BranchContainer.jsx";
+import ButtonContainer from "@/page/ButtonContainer/ButtonContainer.jsx";
 import { ChakraProvider } from '@chakra-ui/react';
 import React, { useState } from 'react';
 
